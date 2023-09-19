ffbc845a1d5e72bb9b57b5c6bb3baf3fab615b2b
Mike <vivancomayta@gmail.com>
2023-09-18 20:27:16 -0500

    validation updated

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,23 +1,25 @@
-import { string, object, minLength, endsWith, email, type Output } from "valibot";
+import { string, object, minLength, maxLength, regex, email, type Output } from "valibot";
 
 export const RegisterSchema = object({
   email: string([
     minLength(1, "Se requiere almenos 1 caracter."),
-    endsWith(
-      "@gmail.com" || "@hotmail.com",
-      "Dominio de correo electrónico inválido."
-    ),
     email("Ingrese un email válido."),
   ]),
   gender: string(),
   password: string([
     minLength(7, "Se requieren almenos 7 caracteres para la contraseña."),
+    maxLength(16, "La contraseña es demasiado larga."),
+    regex(/^[A-Z0-9]+$/i, "La contraseña solo puede tener letras y números."),
   ]),
   password2: string([
     minLength(7, "Se requieren almenos 7 caracteres para la constraseña."),
+    maxLength(16, "La contraseña es demasiado larga."),
+    regex(/^[A-Z0-9]+$/i, "La contraseña solo puede tener letras y números."),
   ]),
   username: string([
     minLength(3, "Se requieren almenos 3 caracteres para el nombre de usuario."),
+    maxLength(12, "El nombre de usuario es demasiado largo."),
+    regex(/^[A-Z0-9]+$/i, "El nombre de usuario solo puede tener letras y números."),
   ]),
 });
 
